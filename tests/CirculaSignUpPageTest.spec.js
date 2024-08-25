653d4ece1a48adcbf5060ef57d0f91c08da64db4
Raksha1998 <shreerakshadesai@gmail.com>
2024-08-25 19:46:30 +0200

    first commit

---
+++ tests/CirculaSignUpPageTest.spec.js
@@ -0,0 +1,25 @@
+const {test, expect } = require('@playwright/test');
+
+test('Circula sign up page dropdown test',async ({page})=>{
+
+    //Step 1: Navigate to sign-up page.
+    await page.goto('https://app.circula.com/users/sign_up');
+    
+    // Accept cookies 
+    const acceptCookiesButton = page.locator('text=Accept All');
+    await acceptCookiesButton.click();
+    
+    // Step 2: Locate ““Where’s your company registered?”- dropdown.
+    const dropdown = await page.getByText("Where’s your company registered?");
+    
+    // Step 3: Click on the dropdown.
+    await dropdown.click();
+    
+    // Step 4: Fill the dropdown with value “Sweden”.
+    await dropdown.fill('Sweden');
+    
+    // Step 5: Verify that "Sweden" is selected in the input field
+    const selectedCountry = page.locator('input#registration-country-input');
+    await expect(selectedCountry).toHaveValue('Sweden');
+
+});
